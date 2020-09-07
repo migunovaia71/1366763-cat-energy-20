053a28a48a12a1bbb6f080d894ebe69afa1d1ba1
migunovaia71 <migunovaia71@gmail.com>
2020-09-07 23:48:52 +0300

    Файлы для публикации

---
+++ docs/js/menu.js
@@ -0,0 +1,2 @@
+var navMain=document.querySelector(".nav"),navToggle=document.querySelector(".nav__toggle");navMain.classList.remove("nav--nojs"),navToggle.addEventListener("click",(function(){navMain.classList.contains("nav--closed")?(navMain.classList.remove("nav--closed"),navMain.classList.add("nav--opened")):(navMain.classList.add("nav--closed"),navMain.classList.remove("nav--opened"))}));
+//# sourceMappingURL=menu.js.map
